9efa579d1d1fe1a5d9faec69cf23dd26167ed8e6
Kees Hink <keeshink@gmail.com>
2015-01-24 18:08:34 +0100

    Save nasty_tags in plone tool (update valid_tags as well)

--- Products/CMFPlone/controlpanel/browser/filter.py
+++ Products/CMFPlone/controlpanel/browser/filter.py
@@ -14,6 +14,30 @@ class FilterControlPanelForm(RegistryEditForm):
     schema = IFilterSchema
     schema_prefix = "plone"
 
+    def _settransform(self, **kwargs):
+        # Cannot pass a dict to set transform parameters, it has
+        # to be separate keys and values
+        # Also the transform requires all dictionary values to be set
+        # at the same time: other values may be present but are not
+        # required.
+        safe_html = getattr(
+            getToolByName(self.context, 'portal_transforms'),
+            'safe_html',
+            None)
+        for k in ('valid_tags', 'nasty_tags'):
+            if k not in kwargs:
+                kwargs[k] = safe_html.get_parameter_value(k)
+
+        for k in list(kwargs):
+            if isinstance(kwargs[k], dict):
+                v = kwargs[k]
+                kwargs[k + '_key'] = v.keys()
+                kwargs[k + '_value'] = [str(s) for s in v.values()]
+                del kwargs[k]
+        safe_html.set_parameters(**kwargs)
+        safe_html._p_changed = True
+        safe_html.reload()
+
     def updateActions(self):  # NOQA
         """Have to override this because we only have Save, not Cancel
         """
@@ -33,6 +57,15 @@ class FilterControlPanelForm(RegistryEditForm):
             safe_html._config['disable_transform'] = disable_filtering
             safe_html._p_changed = True
             safe_html.reload()
+        nasty_tags = data['nasty_tags']
+        if sorted(nasty_tags) != \
+                sorted(safe_html._config['nasty_tags'].keys()):
+            values = dict.fromkeys(nasty_tags, 1)
+            valid = safe_html.get_parameter_value('valid_tags')
+            for value in values:
+                if value in valid:
+                    del valid[value]
+            self._settransform(nasty_tags=values, valid_tags=valid)
 
         # Proceed to registry storage
         if errors:
