c1fbc6577a9b4a3cc664142871979bd56fad1c22
esteele <eric@esteele.net>
2015-09-14 17:32:58 +0200

    Move external_links_open_new_window to the registry

--- Products/CMFPlone/interfaces/controlpanel.py
+++ Products/CMFPlone/interfaces/controlpanel.py
@@ -1011,6 +1011,12 @@ class ISiteSchema(Interface):
                        _('For authenticated users only'))]),
         required=True)
 
+    external_links_open_new_window = schema.Bool(
+        title=_(u'Open external links in new a window'),
+        description=_(u''),
+        default=False,
+        required=False)
+
     toolbar_position = schema.Choice(
         title=_(u'Position where the toolbar is displayed'),
         description=_(
--- Products/CMFPlone/patterns/__init__.py
+++ Products/CMFPlone/patterns/__init__.py
@@ -1,6 +1,7 @@
 from borg.localrole.interfaces import IFactoryTempFolder
 from Products.CMFPlone.interfaces import IPatternsSettings
 from Products.CMFPlone.interfaces import ITinyMCESchema
+from Products.CMFPlone.interfaces import ISiteSchema
 from zope.interface import implements
 from plone.registry.interfaces import IRegistry
 from zope.component import getUtility
@@ -172,10 +173,14 @@ class PloneSettingsAdapter(object):
         properties = getToolByName(self.context, "portal_properties")
         props = getattr(properties, 'site_properties')
 
+        registry = getUtility(IRegistry)
+        settings = registry.forInterface(
+            ISiteSchema, prefix="plone", check=False)
+
         if not props:
             return result
         msl = props.getProperty('mark_special_links', 'false')
-        elonw = props.getProperty('external_links_open_new_window', 'false')
+        elonw = settings.external_links_open_new_window
         if msl == 'true' or elonw == 'true':
             result = {'data-pat-markspeciallinks':
                       ('{"external_links_open_new_window": "%s",'
