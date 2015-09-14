ff79ca52ec324395ccda1b56846cc3a41b22654e
esteele <eric@esteele.net>
2015-09-14 17:32:58 +0200

    Move external_links_open_new_window to the registry

--- Products/CMFPlone/interfaces/controlpanel.py
+++ Products/CMFPlone/interfaces/controlpanel.py
@@ -1023,6 +1023,12 @@ class ISiteSchema(Interface):
                        _('For authenticated users only'))]),
         required=False)
 
+    external_links_open_new_window = schema.Bool(
+        title=_(u'Open external links in new a window'),
+        description=_(u''),
+        default=False,
+        required=False)
+
     toolbar_position = schema.Choice(
         title=_(u'Position where the toolbar is displayed'),
         description=_(
