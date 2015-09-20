a6a1c75d35c58037179b6f0dd2a267718cc0bb3c
Jure Cerjak <jcerjak@termitnjak.si>
2015-09-20 12:32:17 +0200

    make a general base edit form which handles also non-registry fields

--- Products/CMFPlone/controlpanel/browser/security.py
+++ Products/CMFPlone/controlpanel/browser/security.py
@@ -11,7 +11,6 @@ from plone.app.registry.browser import controlpanel
 from plone.registry.interfaces import IRegistry
 from zope.component import getUtility
 from zope.interface import alsoProvides
-from zope.site.hooks import getSite
 
 import logging
 
@@ -19,28 +18,84 @@ logger = logging.getLogger('Products.CMFPlone')
 
 
 class ContextProxy(object):
-    def __init__(self, record_proxy=None, interfaces=None):
+    def __init__(self, record_proxy=None, interfaces=None, form=None):
         self.__record_proxy = record_proxy
+        self.__form = form
         alsoProvides(self, *interfaces)
 
     def __getattr__(self, name):
         if name.startswith('__') or name.startswith('_ContextProxy__'):
             return object.__getattr__(self, name)
-        if name == 'enable_self_reg':
-            return self.__get_enable_self_reg()
-        else:
+        if name in self.__record_proxy.__schema__:
             return getattr(self.__record_proxy, name)
+        else:
+            return getattr(self.__form, name)
 
     def __setattr__(self, name, value):
         if name.startswith('__') or name.startswith('_ContextProxy__'):
             return object.__setattr__(self, name, value)
-        elif name == 'enable_self_reg':
-            return self.__set_enable_self_reg(value)
-        else:
+        if name in self.__record_proxy.__schema__:
             return setattr(self.__record_proxy, name, value)
+        else:
+            return setattr(self.__form, name, value)
 
-    def __get_enable_self_reg(self):
-        portal = getSite()
+
+class RegistryProxyEditForm(controlpanel.RegistryEditForm):
+    """Registry edit form that can contain fields that are not part of
+    the registry schema.
+
+    This form base class is useful if your form needs to work with fields that
+    are not part of the registry. First define a schema with fields that will
+    be stored in the registry (see ``RegistryEditForm`` for more information).
+    Then define a separate schema with fields which will not be part of
+    the registry. This schema should extend the registry schema::
+
+      class IExtendedSettings(IMySettings):
+          # define additional non-registry fields here
+          extra_field = schema.TextLine(title=u'Extra field')
+
+    Your form class should subclass this form and define the ``schema`` and
+    ``extended_schema``class attributes, pointing to the relevant interfaces::
+
+      class MyForm(RegistryProxyEditForm):
+          schema = IMySettings
+          extended_schema = IExtendedSettings
+
+    To handle the additional fields in the ``IExtendedSettings`` schema,
+    you need to define a property on your form class that gets and sets the
+    field value (property name has to be the same as the name of the field)::
+
+      class MyForm(RegistryProxyEditForm):
+          schema = IMySettings
+          extended_schema = IExtendedSettings
+
+          def get_extra_field(self):
+              # get the field value from some location other than the registry
+
+          def set_extra_field(self, value):
+              # set the field value to some location other than the registry
+
+          extra_field = property(get_extra_field, set_extra_field)
+    """
+    def getContent(self):
+        record_proxy = getUtility(IRegistry).forInterface(
+            self.registry_schema,
+            prefix=self.schema_prefix
+        )
+        interfaces = [self.schema]
+        return ContextProxy(record_proxy=record_proxy, interfaces=interfaces,
+                            form=self)
+
+
+class SecurityControlPanelForm(RegistryProxyEditForm):
+    id = "SecurityControlPanel"
+    label = _(u"Security Settings")
+    registry_schema = ISecuritySchema
+    schema = IExtendedSecuritySchema
+    schema_prefix = "plone"
+
+    def get_enable_self_reg(self):
+        portal = self.context
         app_perms = portal.rolesOfPermission(
             permission='Add portal member')
         for app_perm in app_perms:
@@ -49,8 +104,8 @@ class ContextProxy(object):
                 return True
         return False
 
-    def __set_enable_self_reg(self, value):
-        portal = getSite()
+    def set_enable_self_reg(self, value):
+        portal = self.context
         app_perms = portal.rolesOfPermission(
             permission='Add portal member')
         reg_roles = []
@@ -66,21 +121,7 @@ class ContextProxy(object):
         portal.manage_permission('Add portal member', roles=reg_roles,
                                  acquire=0)
 
-
-class SecurityControlPanelForm(controlpanel.RegistryEditForm):
-    id = "SecurityControlPanel"
-    label = _(u"Security Settings")
-    schema = IExtendedSecuritySchema
-    registry_schema = ISecuritySchema
-    schema_prefix = "plone"
-
-    def getContent(self):
-        record_proxy = getUtility(IRegistry).forInterface(
-            self.registry_schema,
-            prefix=self.schema_prefix
-        )
-        interfaces = [self.registry_schema, self.schema]
-        return ContextProxy(record_proxy=record_proxy, interfaces=interfaces)
+    enable_self_reg = property(get_enable_self_reg, set_enable_self_reg)
 
 
 class SecurityControlPanel(controlpanel.ControlPanelFormWrapper):
